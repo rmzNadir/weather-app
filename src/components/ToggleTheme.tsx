eae56697916fb374541cabb2f8bdd1254e114b2d
rmzNadir <dgzrz99@gmail.com>
2021-10-26 22:42:06 -0500

    :bug: Fixed default theme color

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -6,61 +6,37 @@ import {
   IconButton,
   useTheme,
   useColorModeValue,
-  ColorMode,
 } from 'native-base';
 import { ActivityIndicator } from 'react-native-paper';
 
-interface ToggleButtonProps {
-  toggleColorMode(): void;
-  loading: boolean;
-  setLoading(loading: boolean): void;
-  colorMode: ColorMode;
-}
-
-export const ToggleButton = ({
-  toggleColorMode,
-  loading,
-  setLoading,
-  colorMode,
-}: ToggleButtonProps) => {
-  const { colors } = useTheme();
-  const color = useColorModeValue(colors.lightText, colors.darkText);
-
-  return (
-    <IconButton
-      onPress={() => {
-        setLoading(true);
-        toggleColorMode();
-      }}
-      borderRadius='full'
-      variant='solid'
-      _pressed={{ backgroundColor: loading ? 'primary.500' : undefined }}
-      _icon={{
-        size: 'sm',
-        as: FontAwesome5,
-        name: loading ? undefined : colorMode === 'light' ? 'sun' : 'moon',
-      }}>
-      {loading ? <ActivityIndicator size='small' color={color} /> : undefined}
-    </IconButton>
-  );
-};
-
 const ToggleTheme = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { colors } = useTheme();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(false);
   }, [colorMode]);
 
+  const color = useColorModeValue(colors.lightText, colors.darkText);
+
   return (
     <HStack space={2} alignItems='center'>
-      <ToggleButton
-        toggleColorMode={toggleColorMode}
-        loading={loading}
-        setLoading={setLoading}
-        colorMode={colorMode}
-      />
+      <IconButton
+        onPress={() => {
+          setLoading(true);
+          toggleColorMode();
+        }}
+        borderRadius='full'
+        variant='solid'
+        _pressed={{ backgroundColor: loading ? 'primary.500' : undefined }}
+        _icon={{
+          size: 'sm',
+          as: FontAwesome5,
+          name: loading ? undefined : colorMode === 'light' ? 'sun' : 'moon',
+        }}>
+        {loading ? <ActivityIndicator size='small' color={color} /> : undefined}
+      </IconButton>
     </HStack>
   );
 };
